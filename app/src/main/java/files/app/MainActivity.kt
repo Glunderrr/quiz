241d244a9a804eeaa54325e00e26ab9b1d1422c3
Glunderrr <okfvsu@gmail.com>
2023-08-04 20:13:15 +0300

    test_branch first commit

--- app/src/main/java/files/app/MainActivity.kt
+++ app/src/main/java/files/app/MainActivity.kt
@@ -3,11 +3,12 @@ package files.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import files.app.ui.theme.QuizTheme
@@ -17,7 +18,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QuizTheme {
+                HelloWorld()
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorld() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)) {
+        Text(text = "Hello world")
     }
 }
