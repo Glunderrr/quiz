c2540a3d809febf366b4c4217ef3d91f5e158254
Glunderrr <okfvsu@gmail.com>
2024-12-17 11:56:21 +0200

    commit

--- app/src/main/java/files/app/MainActivity.kt
+++ app/src/main/java/files/app/MainActivity.kt
@@ -3,33 +3,16 @@ package files.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import files.app.ui.theme.QuizTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val db =
         setContent {
             QuizTheme {
-                HelloWorld()
+
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HelloWorld() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)) {
-        Text(text = "Hello world")
     }
 }
